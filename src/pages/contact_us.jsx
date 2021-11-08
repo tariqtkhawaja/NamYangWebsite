2a8758fd9a2c8000f087d56ce043cac43eff2fb8
Tariq Khawaja <tariqkhawaja26@live.com>
2021-11-08 13:03:18 +0000

    google map rendered

--- src/pages/contact_us.jsx
+++ src/pages/contact_us.jsx
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import React, { Component, useState } from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from "react-google-maps";
 
 // This is where we'll input the google map
 function Map() {
+  const [selectedMarker, setSelectedMarker] = useState(null);
   return (
     <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 50.82321, lng: -0.35754 }}
-    />
+      defaultZoom={15}
+      defaultCenter={{ lat: 50.823212, lng: -0.35754 }}
+    >
+      <Marker
+        position={{ lat: 50.823212, lng: -0.35754 }}
+        onClick={() => {
+          setSelectedMarker();
+        }}
+      />
+    </GoogleMap>
   );
 }
 
@@ -19,7 +32,7 @@ export class ContactUs extends Component {
       <div style={{ width: "100vw", height: "40vh" }}>
         <WrappedMap
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
-          libraries=geometry,drawing,places&key=AIzaSyCN47v8fwH3rxI4CR4TuAH_42pSk2uaE9o`}
+          libraries=geometry,drawing,places&key=AIzaSyCsQkrNyQJsoji1MI0Cwci2h5gK4HJJtfo`}
           loadingElement={<div style={{ height: "100%" }} />}
           containerElement={<div style={{ height: "100%" }} />}
           mapElement={<div style={{ height: "100%" }} />}
